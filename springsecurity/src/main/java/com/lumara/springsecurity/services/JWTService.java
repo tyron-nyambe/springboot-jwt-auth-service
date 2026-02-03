a5b3230674224dc72cfa855015204925ce39e76c
ty <tyronnyambe@gmail.com>
2026-02-04 00:06:03 +0200

    Add JWTService interface for token generation, validation, and claim extraction

--- springsecurity/src/main/java/com/lumara/springsecurity/services/JWTService.java
+++ springsecurity/src/main/java/com/lumara/springsecurity/services/JWTService.java
@@ -1,4 +1,12 @@
 package com.lumara.springsecurity.services;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface JWTService {
+
+    String extractUsername(String token);
+
+    String generateToken(UserDetails userDetails);
+
+    boolean isTokenValid(String token, UserDetails userDetails);
 }
