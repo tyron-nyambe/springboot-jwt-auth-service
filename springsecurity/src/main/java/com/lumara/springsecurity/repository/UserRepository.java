c509ec43389f11cbd5dfe2cf62e8c21353ebf082
ty <tyronnyambe@gmail.com>
2026-02-04 00:05:22 +0200

    Create UserRepository with JPA and custom findByEmail query

--- springsecurity/src/main/java/com/lumara/springsecurity/repository/UserRepository.java
+++ springsecurity/src/main/java/com/lumara/springsecurity/repository/UserRepository.java
@@ -5,6 +5,11 @@ import com.lumara.springsecurity.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
+
+    Optional<User> findByEmail(String email);
 }
