d7756dfc736da6413f2ec712ae345e0d4216ee2b
ty <tyronnyambe@gmail.com>
2026-02-20 17:29:04 +0200

    Implemented logic for the sign up api

--- springsecurity/src/main/java/com/lumara/springsecurity/controller/AuthenticationController.java
+++ springsecurity/src/main/java/com/lumara/springsecurity/controller/AuthenticationController.java
@@ -1,6 +1,12 @@
 package com.lumara.springsecurity.controller;
 
+import com.lumara.springsecurity.dto.SignUpRequest;
+import com.lumara.springsecurity.entities.User;
+import com.lumara.springsecurity.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
+    //Injecting authentication service
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@RequestBody SignUpRequest signUpRequest)
+    {
+        return ResponseEntity.ok(authenticationService.signUp(signUpRequest));
+    }
 }
