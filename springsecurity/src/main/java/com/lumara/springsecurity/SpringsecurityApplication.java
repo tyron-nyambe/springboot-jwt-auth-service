00bc78eac76e34324ee242b1b9f1ecdfd0e1acbc
ty <tyronnyambe@gmail.com>
2026-02-20 17:29:40 +0200

    Added logic to create admin account on startup of the api

--- springsecurity/src/main/java/com/lumara/springsecurity/SpringsecurityApplication.java
+++ springsecurity/src/main/java/com/lumara/springsecurity/SpringsecurityApplication.java
@@ -1,13 +1,40 @@
 package com.lumara.springsecurity;
 
+import com.lumara.springsecurity.entities.Role;
+import com.lumara.springsecurity.entities.User;
+import com.lumara.springsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SpringsecurityApplication {
+public class SpringsecurityApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringsecurityApplication.class, args);
 	}
 
+
+    public void run(String... args){
+        User adminAccount = userRepository.findByRole(Role.ADMIN);
+
+        //Create another user only if admin account does not exist
+        if(adminAccount == null){
+            User user = new User();
+
+            user.setEmail("admin@gmail.com");
+            user.setFirstname("admin");
+            user.setSecondname("admin");
+            user.setRole(Role.ADMIN);
+            user.setPassword(new BCryptPasswordEncoder().encode("admin"));
+
+            userRepository.save(user);
+        }
+    }
 }
