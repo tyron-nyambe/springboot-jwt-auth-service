7c48ecb55bbe05f557046906bf64ec7767072173
ty <tyronnyambe@gmail.com>
2026-02-20 17:30:13 +0200

    Added find by role method

--- springsecurity/src/main/java/com/lumara/springsecurity/repository/UserRepository.java
+++ springsecurity/src/main/java/com/lumara/springsecurity/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.lumara.springsecurity.repository;
 
 
+import com.lumara.springsecurity.entities.Role;
 import com.lumara.springsecurity.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,4 +13,7 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
 
     Optional<User> findByEmail(String email);
+
+
+    User findByRole(Role role);
 }
